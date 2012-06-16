99e1aa0ceaf924b2f83ee67807b2dcd801a657a3
Daniel Wood <daniel.l.wood@gmail.com>
2012-06-15 23:20:52 -0400

    Cleaning

--- rp/writer/writer.py
+++ rp/writer/writer.py
@@ -35,25 +35,5 @@ class Writer(object):
         file_handle = open(self.output_file, 'w')
         
         # Write file header
-        file_handle.write(struct.pack('<II', header.signature, header.version))
-        
-        for policy in registry_pol.body.policies:
-            file_handle.write(POLICY_ENTER_DELIM)
-            file_handle.write(policy.key + POLICY_SECTION_TERM)
-            file_handle.write(POLICY_SECTION_DELIM)
-            file_handle.write(policy.value + POLICY_SECTION_TERM)
-            file_handle.write(POLICY_SECTION_DELIM)
-            file_handle.write(struct.pack('<I', policy.regtype))
-            file_handle.write(POLICY_SECTION_DELIM)
-            file_handle.write(struct.pack('<I', policy.size))
-            file_handle.write(POLICY_SECTION_DELIM)
-            
-            if policy.regtype == 1:
-                file_handle.write(policy.data)
-                file_handle.write(POLICY_SECTION_TERM)
-            elif policy.regtype == 4:
-                file_handle.write(struct.pack('<I', policy.data))
-            
-            file_handle.write(POLICY_EXIT_DELIM)
-        
+        file_handle.write(str(registry_pol))
     
